5bd11954bf38a7801695ae95799e17c08200add4
Rahul <2025.rahul.panchal@ves.ac.in>
2026-02-25 14:36:00 +0530

    Initial Factorial App

---
+++ src/main/java/Factorial.java
@@ -0,0 +1,22 @@
+public class Factorial {
+
+    public static long findFactorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Number must be non-negative");
+        }
+
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int number = 5;   // Change this to test
+        long fact = findFactorial(number);
+
+        System.out.println("Factorial of " + number + " is: " + fact);
+    }
+}
